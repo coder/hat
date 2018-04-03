5f9add24cb8d635c54a386e136ae6f1c37c0adc2
Ammar Bandukwala <ammar@ammar.io>
2018-04-03 10:34:18 -0500

    Use strings.Trim instead of TrimPrefix and TrimSuffix
    Resolves #3

--- urljoin.go
+++ urljoin.go
@@ -2,11 +2,12 @@ package hat
 
 import "strings"
 
+// urlJoin is used instead of a more formal url.URL concat so
+// that multiple paths
 func urlJoin(elems ...string) string {
 	var u strings.Builder
 	for i, e := range elems {
-		e = strings.TrimPrefix(e, "/")
-		e = strings.TrimSuffix(e, "/")
+		e = strings.Trim(e, "/")
 
 		u.WriteString(e)
 		if i != len(elems)-1 {
